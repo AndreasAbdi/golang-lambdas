83489b9a0888d0e3e3c880bb037d5da0db99c2ea
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-07-18 22:57:32 -0700

    Fixed issue with s3 write via golang. shifted position from writing to temp file. need to reopen file to read

--- five/test_s3.go
+++ five/test_s3.go
@@ -12,27 +12,31 @@ import (
 
 const testBucketName = "aabdi.lambda.test"
 const testBucketKey = "test.key.element"
+const testRegion = "us-east-1"
 
 func check(e error) {
 	if e != nil {
+		fmt.Printf("Error occured!!")
 		panic(e)
 	}
 }
 
-func createFile(fileContent string) *os.File {
+func createFile(fileContent string) string {
 	data := []byte(fileContent)
 	tempFile, err := ioutil.TempFile("", "")
 	check(err)
 	_, err = tempFile.Write(data)
 	check(err)
-	return tempFile
+	fmt.Printf("wrote to file named (%s): %s\n", tempFile.Name(), string(data))
+	tempFile.Sync()
+	return tempFile.Name()
 }
 
-func main() {
-	file := createFile("some_text")
-	defer file.Close()
-
-	activeSession := session.Must(session.NewSession())
+func uploadFile(filename string) (*s3manager.UploadOutput, error) {
+	file, err := os.Open(filename)
+	check(err)
+	awsConfig := aws.NewConfig().WithRegion(testRegion)
+	activeSession := session.Must(session.NewSession(awsConfig))
 	uploader := s3manager.NewUploader(activeSession)
 
 	result, err := uploader.Upload(&s3manager.UploadInput{
@@ -41,8 +45,13 @@ func main() {
 		Body:   file,
 	})
 
-	check(err)
+	return result, err
+}
 
+func main() {
+	file := createFile("some_text")
+	result, err := uploadFile(file)
+	check(err)
 	fmt.Printf("File uploaded to, %s\n", result.Location)
 
 }
